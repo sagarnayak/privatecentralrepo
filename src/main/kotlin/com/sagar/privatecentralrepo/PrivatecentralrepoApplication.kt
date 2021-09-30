6164dc13b774df05a267553a36e3ec96591e0fef
Stark <sagar@agpaytech.co.uk>
2021-09-30 15:43:13 +0530

    udate

---
+++ src/main/kotlin/com/sagar/privatecentralrepo/PrivatecentralrepoApplication.kt
@@ -0,0 +1,11 @@
+package com.sagar.privatecentralrepo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrivatecentralrepoApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrivatecentralrepoApplication>(*args)
+}
---
+++ src/test/kotlin/com/sagar/privatecentralrepo/PrivatecentralrepoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sagar.privatecentralrepo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrivatecentralrepoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
